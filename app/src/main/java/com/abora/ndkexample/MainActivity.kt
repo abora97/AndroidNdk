4d7d5d0a4e86b1233a520b7d5577c26286402af3
Abora <mahmod.elebiary@gmail.com>
2023-02-20 01:14:27 +0200

    make sample Calculator

--- app/src/main/java/com/abora/ndkexample/MainActivity.kt
+++ app/src/main/java/com/abora/ndkexample/MainActivity.kt
@@ -8,7 +8,8 @@ import com.abora.ndkexample.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    var num1 = ""
+    var num2 = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,13 +18,59 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
+
+        clicks()
     }
+
+
+    private fun clicks() {
+        binding.btnAdd.setOnClickListener {
+            getNumFromUi()
+            if (checkNumbrs(num1, num2)) {
+                binding.sampleText.text = add(num1.toInt(), num2.toInt()).toString()
+            }
+        }
+        binding.btnSub.setOnClickListener {
+            getNumFromUi()
+            if (checkNumbrs(num1, num2)) {
+                binding.sampleText.text = sub(num1.toInt(), num2.toInt()).toString()
+            }
+        }
+        binding.btnMultiply.setOnClickListener {
+            getNumFromUi()
+            if (checkNumbrs(num1, num2)) {
+                binding.sampleText.text = multiply(num1.toInt(), num2.toInt()).toString()
+            }
+        }
+        binding.btnDivide.setOnClickListener {
+            getNumFromUi()
+            if (checkNumbrs(num1, num2)) {
+                if (num2 != "0")
+                    binding.sampleText.text = divide(num1.toInt(), num2.toInt()).toString()
+            }
+        }
+    }
+
+
+    private fun checkNumbrs(num1: String, num2: String): Boolean {
+        return num1.isNullOrEmpty().not() && num2.isNullOrEmpty().not()
+    }
+
+    private fun getNumFromUi() {
+        num1 = binding.edNum1.text.toString()
+        num2 = binding.edNum2.text.toString()
+    }
+
 
     /**
      * A native method that is implemented by the 'ndkexample' native library,
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+    external fun add(x: Int, y: Int): Int
+    external fun sub(x: Int, y: Int): Int
+    external fun multiply(x: Int, y: Int): Int
+    external fun divide(x: Int, y: Int): Int
 
     companion object {
         // Used to load the 'ndkexample' library on application startup.
